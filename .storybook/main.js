6467d99fa73809ee7ef15b1dff9c3a6075ac4fc3
Ewa <ewahjelm84@gmail.com>
2025-09-15 14:31:04 +0200

    dubbla storybook?

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,21 +3,13 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   "stories": [
-<<<<<<< HEAD
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-=======
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
->>>>>>> 725daaf03e9ab8d2c5517b0c534a95f99db5114f
   ],
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-<<<<<<< HEAD
     "@storybook/addon-onboarding",
-=======
->>>>>>> 725daaf03e9ab8d2c5517b0c534a95f99db5114f
     "@storybook/addon-a11y",
     "@storybook/addon-vitest"
   ],
