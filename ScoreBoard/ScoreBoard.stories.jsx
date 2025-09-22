4d1aa68cee94c5a89549a5f9c9ed16901f0e7803
Yusra Aman <Yusra.aman00@gmail.com>
2025-09-22 13:01:39 +0200

    stories file updated

---
+++ ScoreBoard/ScoreBoard.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ScoreBoard from './ScoreBoard';
+
+export default{
+
+	title: 'Components/ScoreBoard',
+	component: ScoreBoard,
+};
+
+export const Default = { args: { message: 'Antal vunna matcher'  } };
+
