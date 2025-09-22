6893a98d9388afdaeb7078e421b5ca8fe72ca369
Yusra Aman <Yusra.aman00@gmail.com>
2025-09-22 13:14:59 +0200

    ScoreBoard.jsx uppdaterad

--- ScoreBoard/ScoreBoard.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-//import Result from "./components/Result";
-//import Reset from "./components/reset"; 
-
-export default ScoreBoard;
---
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -0,0 +1,7 @@
+//import Result from "./components/Result";
+//import Reset from "./components/reset"; 
+
+import React from "react";
+export default function Score({ wonGame = 1 }) {
+  return <div>Matcher vunna: {wonGame}</div>;
+}
